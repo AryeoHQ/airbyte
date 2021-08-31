1acad66caea8106a0f0dad6cec0cb07c1c3a417f
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-08-31 17:28:36 -0500

    test increment syncing with bridge api

--- airbyte-integrations/connectors/source-bridge/source_bridge/source.py
+++ airbyte-integrations/connectors/source-bridge/source_bridge/source.py
@@ -23,6 +23,7 @@
 #
 
 from abc import ABC
+from datetime import date
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import pendulum
@@ -35,22 +36,36 @@ from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 from datetime import datetime, timedelta
 
-class BridgeStream(HttpStream):
+class BridgeStream(HttpStream, ABC):
     url_base = "https://api.bridgedataoutput.com/api/v2/OData/"
-    
-    primary_key = 'ListingKey'
 
-    def __init__(self, dataset: str, brokerage_key: str, **kwargs):
-        super().__init__(**kwargs)
+    primary_key = 'ListingKey'
+    cursor_field = 'updated_at'
+
+    datetime_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+    def __init__(
+        self,
+        dataset: str,
+        brokerage_key: str,
+        **kwargs
+    ):
+        super(BridgeStream, self).__init__(**kwargs)
         self.dataset = dataset
         self.brokerage_key = brokerage_key        
 
     def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None
     ) -> str:
         return f'{self.dataset}/Property'
 
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+    def next_page_token(
+        self,
+        response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
         response_json = response.json()
         if response_json.get("@odata.nextLink"):
             next_query_string = urllib.parse.urlsplit(response_json.get("@odata.nextLink")).query
@@ -78,22 +93,47 @@ class BridgeStream(HttpStream):
         self,
         response: requests.Response,
         stream_state: Mapping[str, Any],
-        stream_slice: Mapping[str, Any] = None,
-        next_page_token: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None
     ) -> Iterable[Mapping]:
         json_response = response.json()
         for record in json_response["value"]:
-            yield record
+            if stream_state:
+                if record['BridgeModificationTimestamp'] >= stream_state.get(self.cursor_field):
+                    yield record
+            else:
+                yield record
+
+    def get_updated_state(
+        self,
+        current_stream_state: MutableMapping[str, Any],
+        latest_record: Mapping[str, Any]
+    ) -> Mapping[str, any]:
+        # This method is called once for each record returned from the API.
+        # Compare each record's timestamp to the cursor value in the current state.
+        # If this is the first time we run a sync or no state was passed, current_stream_state will be None.
+        if current_stream_state is not None and 'updated_at' in current_stream_state:
+            current_parsed_date = datetime.strptime(current_stream_state['updated_at'], self.datetime_format)
+            latest_record_date = datetime.strptime(latest_record['BridgeModificationTimestamp'], self.datetime_format)
+            return {'updated_at': max(current_parsed_date, latest_record_date).strftime(self.datetime_format)}
+        else:
+            return {'updated_at': date.today().strftime(self.datetime_format)}
 
 class SourceBridge(AbstractSource):
-    def check_connection(self, logger, config) -> Tuple[bool, any]:
+    def check_connection(
+        self,
+        logger,
+        config
+    ) -> Tuple[bool, any]:
         if config["server_token"].isalnum():
             authenticator = TokenAuthenticator(config["server_token"])
             return True, None
         else:
             return False, f"Server token should be alphanumeric."
 
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+    def streams(
+        self,
+        config: Mapping[str, Any]
+    ) -> List[Stream]:
         authenticator = TokenAuthenticator(config["server_token"])
         args = {"authenticator": authenticator, "dataset": config["dataset"], "brokerage_key": config["brokerage_key"]}
         return [
